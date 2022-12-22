b0774f496784064dcfae87dac1dcd99da75b0820
chenxh <x@x.xxx>
2022-12-22 18:01:24 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+fn main() {
+    let url = "https://www.baidu.com";
+    let contents = get_url(url);
+
+    let output = "baidu.html";
+
+    fs::write(output, contents).unwrap();
+
+}
+
+
+
+fn get_url(url:&str)->String{
+    return reqwest::blocking::get(url).unwrap().text().unwrap()
+}
